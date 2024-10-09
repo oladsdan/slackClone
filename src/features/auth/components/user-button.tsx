d068e617d218bee606c3d0c1594506e20c6721fa
OladsDan <kalloello@gmail.com>
2024-10-09 09:17:29 +0100

    done with authenticattion creating a workspce next

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -1,22 +1,40 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { useCurrentUser } from "../hooks/useCurrentUser";
+import { Loader, LogOut } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 
 export const UserButton = () => {
+    const {data, isLoading} = useCurrentUser();
+    const { signOut } = useAuthActions()
+
+    if(isLoading){
+        return <Loader className="size-4 animate-spin text-muted-foreground " />
+    }
+    if(!data){
+        return null;
+    }
+
+    const {image, name, email} = data;
+
+    const avatarFallback = name!.charAt(0).toUpperCase()
+
 
     return (
         <DropdownMenu modal={false}>
             <DropdownMenuTrigger className="outline-none relative">
                 <Avatar className="size-10 hover:opacity-75 transition">
-                    <AvatarImage/>
-                    <AvatarFallback>
-
+                    <AvatarImage alt={name} src={image} className="rounded-full w-20" />
+                    <AvatarFallback className="bg-sky-500 text-white">
+                        {avatarFallback}
                     </AvatarFallback>
                 </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="center" side="right" className="w-80">
-                <DropdownMenuItem>
-                    
+                <DropdownMenuItem onClick={() => signOut()} className="h-10 cursor-pointer">
+                    <LogOut className="size-4 mr-2" />
+                    Log out
                 </DropdownMenuItem>
 
 
@@ -24,3 +42,4 @@ export const UserButton = () => {
         </DropdownMenu>
     )
 }
+
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,11 @@
-import { Button } from "@/components/ui/button"
-import { useAuthActions } from "@convex-dev/auth/react"
 import { UserButton } from "@/features/auth/components/user-button"
 
 
 const HomePage = () => {
-  
-  const { signOut } = useAuthActions()
 
   return (
     <div>
-      HomePage
       <UserButton />
-      <Button onClick={() => signOut()}>Sign Out</Button>
-
 
     </div>
   )
